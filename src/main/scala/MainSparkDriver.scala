1b116f080eb346beccccbe36a303bedf9c129e99
jose@wavezoo.com <jose@wavezoo.com>
2014-05-30 14:45:12 +0200

    trye

--- src/main/scala/MainSparkDriver.scala
+++ src/main/scala/MainSparkDriver.scala
@@ -3,6 +3,8 @@ package spark.starter
 import org.apache.spark._
 import SparkContext._
 
+//test
+
 object MainSparkDriver {
   def main(args: Array[String]) {
     val sc = new SparkContext("spark://master:7077", "SparkStarter", System.getenv("SPARK_HOME"), List("target/scala-2.9.3/spark-starter-assembly-0.1.jar"))
